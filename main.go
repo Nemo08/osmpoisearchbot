e820e9958cdc32d926464428642ba4b0aa984373
Nemo08 <bigblacklazycat@gmail.com>
2016-05-31 12:31:45 +0300

    Добавлены комментарии

--- main.go
+++ main.go
@@ -17,24 +17,30 @@ func main() {
 	}
 
 	bot.Debug = true
-
+	
+	// Авторизация бота
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
-
+	
+	// Бесконечно ждем апдейтов от сервера
 	for update := range updates {
 		switch {
+		// Пришло обычное сообщение
 		case update.Message != nil:
 			/*
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "hello")
 				bot.Send(msg)
 			*/
 			break
+		// Пришел inline запрос
 		case update.InlineQuery != nil:
+			// Длина запроса >=3?
 			if len([]rune(update.InlineQuery.Query)) >= 3 {
+				// Обращаемся к osm.ru
 				res, err := http.Get("http://openstreetmap.ru/api/search?q=" + update.InlineQuery.Query + "&st=&accuracy=1&cnt=5&stype=all&lat=" + strconv.FormatFloat(update.InlineQuery.Location.Latitude, 'f', 7, 64) + "&lon=" + strconv.FormatFloat(update.InlineQuery.Location.Longitude, 'f', 7, 64))
 
 				if err != nil {
@@ -45,16 +51,20 @@ func main() {
 
 				resp := new(Places)
 				var resources []interface{}
+				
+				// Разбираем ответ
 				err = json.Unmarshal(body, &resp)
 
 				if err != nil {
 					log.Printf("whoops:", err)
 				}
 
+				// Если ответ не нулевой
 				if len(resp.Matches) != 0 {
 					for k, i := range resp.Matches {
 						log.Printf(i.Name, i.FullName)
 
+						// Формируем меню venue с полученными пои
 						resloc := InlineQueryResultVenue{}
 						resloc.ID = strconv.Itoa(k)
 						if i.Name == "" {
@@ -74,6 +84,7 @@ func main() {
 						resources = append(resources, resloc)
 					}
 				} else {
+					// Формируем плашку "ничего не найдено"
 					resloc := tgbotapi.InlineQueryResultArticle{}
 					resloc.ID = update.InlineQuery.ID
 					resloc.Type = "article"
@@ -82,6 +93,7 @@ func main() {
 					resloc.InputMessageContent = tgbotapi.InputTextMessageContent{Text: "По вашему запросу ничего не найдено"}
 					resources = append(resources, resloc)
 				}
+				// Отправляем меню пользователю
 				ic := tgbotapi.InlineConfig{}
 				ic.InlineQueryID = update.InlineQuery.ID
 				ic.Results = resources
